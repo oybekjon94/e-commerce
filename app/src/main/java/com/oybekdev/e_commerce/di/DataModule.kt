6b6807c5580f8c4d86ea57cb55324f3009270298
khurshiddev <umaribnxattob8008@gmail.com>
2024-04-03 15:53:51 +0900

    feat: add DataModule.kt

---
+++ app/src/main/java/com/oybekdev/e_commerce/di/DataModule.kt
@@ -0,0 +1,31 @@
+package com.oybekdev.e_commerce.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "store")
+
+
+    @Provides
+    @Singleton
+    fun provideGson() = Gson()
+
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context) = context.dataStore
+}
