7c1d0db9b45bd5e79cf3acdf6156a4dde6e399ce
khurshiddev <umaribnxattob8008@gmail.com>
2024-04-03 15:52:04 +0900

    feat: add SingleLiveEvent.kt

---
+++ app/src/main/java/com/oybekdev/e_commerce/presentation/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.oybekdev.e_commerce.presentation.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oybekdev.e_commerce.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
