35211ab388026c9daa3a2caf34386ed77a857c72
power <x.oybek94@gmail.com>
2024-04-09 17:15:59 +0900

    feat:add onboarding screen

---
+++ app/src/main/java/com/oybekdev/e_commerce/presentation/onboarding/OnBoardingViewModel.kt
@@ -0,0 +1,13 @@
+package com.oybekdev.e_commerce.presentation.onboarding
+
+import androidx.lifecycle.ViewModel
+import com.oybekdev.e_commerce.domain.repo.AuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class OnBoardingViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+):ViewModel(){
+
+}
