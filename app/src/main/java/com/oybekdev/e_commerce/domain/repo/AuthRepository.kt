d4e1cd9ec5ec2b142500e4ed8c73b4c8d368cbbb
khurshiddev <umaribnxattob8008@gmail.com>
2024-04-13 17:22:46 +0900

    feat: add in AuthRepository.kt signUp function

--- app/src/main/java/com/oybekdev/e_commerce/domain/repo/AuthRepository.kt
+++ app/src/main/java/com/oybekdev/e_commerce/domain/repo/AuthRepository.kt
@@ -5,4 +5,5 @@ import com.oybekdev.e_commerce.domain.User
 interface AuthRepository {
 
     suspend fun signIn(username: String, password: String)
+    suspend fun signUp(username: String, email:String, password:String)
 }
