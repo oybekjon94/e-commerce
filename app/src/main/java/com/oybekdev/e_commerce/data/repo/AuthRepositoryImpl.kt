8467008f81d0f0694d11cb7b216e6a4b31d45efc
khurshiddev <umaribnxattob8008@gmail.com>
2024-04-13 22:37:03 +0900

    feat: AuthRepositoryImpl.kt
    -add destination function

--- app/src/main/java/com/oybekdev/e_commerce/data/repo/AuthRepositoryImpl.kt
+++ app/src/main/java/com/oybekdev/e_commerce/data/repo/AuthRepositoryImpl.kt
@@ -4,15 +4,23 @@ import com.oybekdev.e_commerce.data.api.auth.AuthApi
 import com.oybekdev.e_commerce.data.api.auth.dto.AuthResponse
 import com.oybekdev.e_commerce.data.api.auth.dto.SignInRequest
 import com.oybekdev.e_commerce.data.api.auth.dto.SignUpRequest
+import com.oybekdev.e_commerce.data.store.OnboardedStore
 import com.oybekdev.e_commerce.data.store.TokenStore
 import com.oybekdev.e_commerce.data.store.UserStore
+import com.oybekdev.e_commerce.domain.model.Destination
 import com.oybekdev.e_commerce.domain.repo.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val authApi: AuthApi,
     private val tokenStore: TokenStore,
-    private val userStore: UserStore
+    private val userStore: UserStore,
+    private val onboardedStore: OnboardedStore
 ) : AuthRepository {
     override suspend fun signIn(username: String, password: String) {
         val request = SignInRequest(username, password)
@@ -27,8 +35,29 @@ class AuthRepositoryImpl @Inject constructor(
         saveUserInfo(response)
     }
 
+    override fun destinationFlow() = channelFlow {
+        suspend fun sendDestination(){
+            when{
+                tokenStore.get() != null -> send(Destination.Home)
+                onboardedStore.get() == true -> send(Destination.Auth)
+                else -> send(Destination.Onboarding)
+            }
+        }
+        launch {
+            tokenStore.getFlow().collectLatest {
+                sendDestination()
+            }
+        }
+        launch {
+            onboardedStore.getFlow().collectLatest {
+                sendDestination()
+            }
+        }
+    }
+
     private suspend fun saveUserInfo(response: AuthResponse) {
         tokenStore.set(response.token)
         userStore.set(response.user)
     }
 }
+
