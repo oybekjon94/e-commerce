c1acf4b6cbe35e0dc775ad5f4064e3d93d6690a6
khurshiddev <umaribnxattob8008@gmail.com>
2024-04-14 16:44:59 +0900

    feat: Add Destination
    add sealed class for home, onboarding, auth destination

---
+++ app/src/main/java/com/oybekdev/e_commerce/domain/model/Destination.kt
@@ -0,0 +1,8 @@
+package com.oybekdev.e_commerce.domain.model
+
+sealed class Destination {
+
+    object Home : Destination()
+    object Onboarding : Destination()
+    object Auth : Destination()
+}
